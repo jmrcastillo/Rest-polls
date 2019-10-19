641272327fb4ec1777433e71ef0ee90a56529505
dm <jm.cass20@gmail.com>
2019-10-19 21:08:21 +0800

    apiviews and urls - modify

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,14 @@
 
 
-from django.conf.urls import url
-from .views import polls_list, polls_detail
-from .apiviews import PollList, PollDetail
+from django.urls import path
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 
 
 urlpatterns = [
-    url(r"^polls/$", PollList.as_view(), name="polls"),
-    url(r"^polls/(?P<slug>\w+)", PollDetail.as_view(), name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(), name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",
+         CreateVote.as_view(),
+         name="create_vote"),
 ]
